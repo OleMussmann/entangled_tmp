88f6476088c99f6a216aef40feaa83dd8bb14876
Ole Mussmann <github+account@ole.mn>
2023-10-16 11:06:45 +0000

    start writing flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "my project description";
+
+  #inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+	{
+	devShells.default = pkgs.mkShell {
+		buildInputs = with pkgs; [
+			poetry
+			python311
+			pipenv
+		];
+		};
+	}
+      );
+}
